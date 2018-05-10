3f8cb1e784ff7e744a5ba5849527a2d9e9bc2485
tballard34 <38401323+tballard34@users.noreply.github.com>
2018-05-10 09:33:28 -0400

    Update CPUPlayer.java

--- Connnect four/src/gameConnect4/CPUPlayer.java
+++ Connnect four/src/gameConnect4/CPUPlayer.java
@@ -1,68 +1,110 @@
 package gameConnect4;
 
-public class BoardPosition {
-	
-	
-	
-	public BoardPosition() {
-		
+public class CPUPlayer{
+
+	private int board[][];//this board is linked to the game on so don't change it. if you do it will affect the game 7x6 
+	private int[] column;
+	private Connect4 game;
+	private int maxDepth;
+
+	public CPUPlayer(Connect4 game, int maxDepth)
+	{
+		this.game = game;
+		this.board = this.game.getBoard();
+		this.maxDepth = maxDepth;
+		column =  game.getColumn();
 	}
-	
-	//returns and boolean array with the possible moves moves as true and the others false
-	//returns empty array if the game is over
-	public boolean[] possibleMoves() {
-		
-		boolean[] temp = new boolean[7];
-		return null;
-	}
-	
-	//checks a single move for a four in a row
-	//returns max int if cpu wins, min int if cpu loses, 0 if nothing
-	public int checkMove(int slot) {
-		
-		if (checkMoveForWin(slot))
-			return Integer.MAX_VALUE;
-		if (checkMoveForLose(slot))
-			return Integer.MIN_VALUE;
+
+	//the logic for the CPU player is written below
+	//return the column it should place piece 0 - 6
+	public int makeMove()
+	{
 		return 0;
 	}
-	
-	//return true if cpu wins from move slot, false otherwise
-	private boolean checkMoveForWin(int slot) {
-		
-		return false;
+
+	public int minimax(BoardPosition currentBoard, int currentDepth, boolean isCPU, int alpha, int beta)
+	{
+		if (currentDepth == maxDepth) {
+			return scoreBoard(currentBoard);
+		}
+
+		if (isCPU) {
+			int bestVal = Integer.MIN_VALUE;
+			boolean[] possibleMoves = currentBoard.possibleMoves();
+
+			for (int slot = 0; slot < possibleMoves.length; slot++) {
+
+				if (!possibleMoves[slot]) continue; //move is not a possible slot
+
+				int val = currentBoard.checkMove(slot);
+				if (val != 0) {
+					if (val > 0) { //val is max int, winning move for cpu must take
+						bestVal = val;
+						break;
+					}
+				}
+
+				else {
+					val = minimax(currentBoard.makeMove(slot), currentDepth + 1, false, alpha, beta);
+				}
+
+				bestVal = Integer.max(val, bestVal);
+				alpha = Integer.max(alpha, bestVal);
+				if (beta <= alpha)
+					break;
+			}
+			return bestVal;
+		}
+
+		else {
+			int bestVal = Integer.MAX_VALUE;
+			boolean[] possibleMoves = currentBoard.possibleMoves();
+
+			for (int slot = 0; slot < possibleMoves.length; slot++) {
+
+				if (!possibleMoves[slot]) continue; //move is not a possible slot
+
+				int val = currentBoard.checkMove(slot);
+				if (val != 0) {
+					if (val < 0) { //val is min int, winning move for player must take
+						bestVal = val;
+						break;
+					}
+				}
+
+				else {
+					val = minimax(currentBoard.makeMove(slot), currentDepth + 1, false, alpha, beta);
+				}
+
+				bestVal = Integer.min(val, bestVal); //min?
+				beta = Integer.min(beta, bestVal);
+				if (beta <= alpha)
+					break;
+			}
+			return bestVal;
+		}
 	}
-	
-	//return true if cpu wins from move slot, false otherwise
-	private boolean checkMoveForLose(int slot) {
-		
-		return false;
+
+	//Heuristic method
+	//ranks the current boardPosition, high int means in favor of CPU, low int means in not in favor of CPU
+	//CPU wins returns max int possible
+	//CPU loses returns min int possible
+	public int scoreBoard(BoardPosition board) {
+
+		boolean[][] winningPositions = board.findWinningPositions();
+		boolean[][] losingPositions = board.findLosingPositions();
+
+		//step 1: check for double ups (two kinds)
+		//double ups on top of each other
+
+		///double ups not on top of each other but at different slots but both bottom layer
+
+		//step 2: check for even/odd threats
+		//find number of odd threats(for cpu) that has no even threats(for player) below
+
+		//find number of even threats(for player) with no odd threats(for cpu) below
+
+		return 0;
 	}
-	
-	//checks the whole board for four in a row
-	//returns true if the game comes to an end, false if not
-	public boolean checkFourInARow() {
-		
-		return false;
-	}
-	
-	//returns the new board position after a move has been made
-	public BoardPosition makeMove(int slot) {
-		
-		return null;
-	}
-	
-	//finds the winning positions for cpu
-	public boolean[][] findWinningPositions() {
-		
-		boolean[][] temp = new boolean[6][7];
-		return temp;
-	}
-	
-	//find the losing positions for cpu
-	public boolean[][] findLosingPositions() {
-		
-		boolean[][] temp = new boolean[6][7];
-		return temp;
-	}
+
 }
