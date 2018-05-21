68d43ef1532af1edf827b41bad706bfc8f636f60
the-real-krush <krush.christopher@gmail.com>
2018-05-21 11:02:00 -0400

    Update CPUPlayer.java

--- Connnect four/src/gameConnect4/CPUPlayer.java
+++ Connnect four/src/gameConnect4/CPUPlayer.java
@@ -6,6 +6,8 @@ public class CPUPlayer{
 	private int[] column;
 	private Connect4 game;
 	private int maxDepth;
+	private final int LOSING_MOVE = Integer.MIN_VALUE + 1;
+	private final int WINNING_MOVE = Integer.MAX_VALUE - 1;
 
 	public CPUPlayer(Connect4 game, int maxDepth)
 	{
@@ -22,11 +24,13 @@ public class CPUPlayer{
 		long red = boardToLong(1);
 		long black = boardToLong(2);
 		int[] moves = new int[7];
+		int alpha = Integer.MIN_VALUE;
+		int beta = Integer.MAX_VALUE;
 		
 		for(int slot = 0; slot < 7; slot++)
 		{
 			if(column[slot] < 0) {
-				moves[slot] = -1000;
+				moves[slot] = Integer.MIN_VALUE;
 				continue;
 			}
     	  
@@ -37,24 +41,29 @@ public class CPUPlayer{
 			long lMove = Long.parseLong(newMove,2);
 			
 			int val = checkMove((red|lMove), black, indexMove ,moves[slot]);
-			//int bestVal = -1000;--------------------------------why do we need this
+			//int bestVal = Integer.MIN_VALUE;
     	  
 			column[slot] = column[slot] - 1;
     	  
 			if (val != 0){
 				if (val > 0) { //val is max int, winning move for cpu must take
-					moves[slot] = 100;
+					moves[slot] = WINNING_MOVE;
 					column[slot] = column[slot] + 1;
-					break;
+					//bestVal = WINNING_MOVE;
 				}
 			}else if(maxDepth == 1)
 				moves[slot] = scoreBoard((red|lMove), black);
 			else{
-				val = minimax((red|lMove), black,column[0],column[1],column[2],column[3],column[4],column[5],column[6] , maxDepth - 1, false, -1000, 1000);
+				moves[slot] = minimax((red|lMove), black,column[0],column[1],column[2],column[3],column[4],column[5],column[6] , maxDepth - 1, false, alpha, beta);
 			}
 			column[slot] = column[slot] + 1;
       }
-      return maxIndex(moves);
+		
+		for(int i = 0; i < moves.length; i++)
+			System.out.println(moves[i]);
+		int index = maxIndex(moves);
+		System.out.println("index" + index);
+      return index;
 	}
 //****************************************minimax method*************************************************
 	public int minimax(long red, long black,int c, int c1, int c2,int c3, int c4, int c5, int c6, int currentDepth, boolean isCPU, int alpha, int beta)
@@ -79,7 +88,7 @@ public class CPUPlayer{
 				long lMove = Long.parseLong(newMove,2);
 
 				int val = checkMove((red|lMove), black, indexMove ,moves[slot]);
-
+				//System.out.println("red" + val);
 				moves[slot] = moves[slot] - 1;
 				
 				if (val != 0) {
@@ -94,9 +103,9 @@ public class CPUPlayer{
 				}
 
 				bestVal = Integer.max(val, bestVal);
-				alpha = Integer.max(alpha, bestVal);
+				/*alpha = Integer.max(alpha, bestVal);
 				if (beta <= alpha)
-					break;
+					break;*/
 			}
 			return bestVal;
 		}
@@ -109,13 +118,13 @@ public class CPUPlayer{
 				if (moves[slot] < 0) continue; //move is not a possible slot
 				
 				int indexMove = moves[slot]*7 + slot;
-				
+				//System.out.println(moves[slot]);
 				String newMove = "000000000000000000000000000000000000000000";
 				newMove = newMove.substring(0,indexMove)+"1" + newMove.substring(indexMove + 1);
 				long lMove = Long.parseLong(newMove,2);
 				
 				int val = checkMove(red, (black|lMove), indexMove, moves[slot]);
-				System.out.println(val);
+				//System.out.println("black" + val);
 				moves[slot] = moves[slot] - 1;
 				
 				if (val != 0) {
@@ -132,10 +141,11 @@ public class CPUPlayer{
 				moves[slot] = moves[slot] + 1;
 				
 				bestVal = Integer.min(val, bestVal); //min?
-				beta = Integer.min(beta, bestVal);
+				/*beta = Integer.min(beta, bestVal);
 				if (beta <= alpha)
-					break;
+					break;*/
 			}
+			//System.out.println(bestVal);
 			return bestVal;
 		}
 	}
@@ -159,57 +169,58 @@ public class CPUPlayer{
 	{
 		if(moveWon(red, black, indexMove, slot))//if there has been a win will check who then return there value
 			if(longToString(red).charAt(indexMove) == '1')
-				return 100;//cpu
+				return WINNING_MOVE;//cpu
 			else
-				return -100;//person
+				return LOSING_MOVE;//person
 		return 0;
 	}
-	
+	/**********************************************************************************************************************/
 	private boolean moveWon(long red, long black, int indexMove, int slot)
 	{
 		char player = '1';
 		String str;
 
-		if(longToString(red).charAt(indexMove) == '1') {//sets up how is being looked at for a win
-			System.out.println("red");
+		if(longToString(red).charAt(indexMove) == '1') {//sets up how its being looked at for a win
+			//System.out.println("red");
 			str = longToString(red);
 		}
 		else{
-			System.out.println("black");
+			//System.out.println("black");
 			str = longToString(black);
 		}
 		
+		//System.out.println(str);
+		
 		int count = 0;//hold the count value of the winning pieces
-		for(int i = 1; i < 4; i++)//checks down
+		for(int i = 0; i < 4; i++)//checks down
 		{
-			if(indexMove - 7*i > 0 && str.charAt(indexMove - 7*i) == player) {
+			if(indexMove + 7*i < str.length() && str.charAt(indexMove + 7*i) == player) {
 				count++;
 			}else
 				break;
 				
 		}
-
-		if(count + 1 == 4)
+		if(count == 4)
 			return true;
       
       
 		count = 0;
 		for(int i = 1; i < 4; i++)//checks diagonal bottom left to top right
 		{
-			if(indexMove - 6*i > 0 && str.charAt(indexMove - 6*i) == player) {
+			if(indexMove - 6*i > 0 && indexMove - 6*i < (slot - i + 1) * 7 && str.charAt(indexMove - 6*i) == player) {
 				count++;
 			}else
 				break;
 		}
 		for(int i = 1; i < 4; i++)//checks diagonal top right to bottom left
 		{
-			if(indexMove + 6*i < str.length() && str.charAt(indexMove + 6*i) == player) {
+			if(indexMove + 6*i < str.length() && indexMove + 6 * i < (slot + i + 1) && str.charAt(indexMove + 6*i) == player) {
 				count++;
 			}else
 				break;
 		}
 		//System.out.println(count);
-		if(count+1 == 4)
+		if(count == 3)
 			return true;
       
       
@@ -228,8 +239,8 @@ public class CPUPlayer{
 			}else
 				break;
 		}
-
-		if(count + 1 == 4)
+		//System.out.println(count);
+		if(count == 3)
 			return true;
          
          
@@ -248,12 +259,10 @@ public class CPUPlayer{
 			}else
 				break;
 		}
-
-		if(count + 1== 4)
+		//System.out.println(count);
+		if(count == 3)
 			return true;
-      
-      
-      
+
 		return false;
 	}
 	
@@ -269,7 +278,7 @@ public class CPUPlayer{
 
 		//find number of even threats(for player) with no odd threats(for cpu) below
 
-		return 4;
+		return 0;
 	}
 	
 	//converts the board in to a bitboard for each player by making the board in to a long value of base two
