6ffc776069c9ca3b4a455b67c8a95d64764d3e8b
the-real-krush <krush.christopher@gmail.com>
2018-05-27 11:23:33 -0400

    Update MouseInput.java

--- Connnect four/src/gameInput/MouseInput.java
+++ Connnect four/src/gameInput/MouseInput.java
@@ -5,38 +5,36 @@ import java.awt.event.MouseEvent;
 
 public class MouseInput extends MouseAdapter{
 	
-	private static final int NUM_BUT = 10;
+	private static final int NUM_BUT = 10;//this is used to set the max amount of buttonls that the mouse can use
 	
-	private static final boolean buttons[] = new boolean[NUM_BUT];
-	private static final boolean lastButton[] = new boolean[NUM_BUT];
+	private static final boolean buttons[] = new boolean[NUM_BUT];//if they had been pushed it makes this true
+	private static final boolean releasedButton[] = new boolean[NUM_BUT];
 	
-	private static int x;
-	private static int y;
+	private static int x;//x position
+	private static int y;//y position
 	
-	public void mousePressed(MouseEvent e){
+	public void mousePressed(MouseEvent e){//sets the pushed button to true
 		System.out.println("Mouse Button: " + e.getButton());
 		buttons[e.getButton()] = true;
 	}
 	
 	public void mouseReleased(MouseEvent e){
-		buttons[e.getButton()] = false;
+		buttons[e.getButton()] = false;//sets it to false because its not being clicked
+      releasedButton[e.getButton()] = true;
 	}
 	
-	public void mouseDragged(MouseEvent e){
-		
-	}
-	
-	public void mouseMoved(MouseEvent e){
+	public void mouseMoved(MouseEvent e){//keeps track of where the mouse is at all times
 		x = e.getX();
 		y = e.getY();
 	}
 	
-	public static void update(){
-		for(int k = 0; k < buttons.length; k++){
-			lastButton[k] = buttons[k];
-		}
-	}
-	
+   public static boolean released(int button)
+   {
+      boolean btn = releasedButton[button];
+      releasedButton[button] = false;
+      return btn;
+   }
+   
 	public static boolean isButtonClicked(int button){
 		return buttons[button] == true;
 	}
